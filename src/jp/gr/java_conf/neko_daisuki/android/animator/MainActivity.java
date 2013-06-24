c351a57a3b947c9629cb3cddf0a9e9e2a9dd74b5
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2013-06-24 23:49:14 +0900

    Enhanced to show preview.

--- src/jp/gr/java_conf/neko_daisuki/android/animator/MainActivity.java
+++ src/jp/gr/java_conf/neko_daisuki/android/animator/MainActivity.java
@@ -1,10 +1,37 @@
 package jp.gr.java_conf.neko_daisuki.android.animator;
 
 import android.app.Activity;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 public class MainActivity extends Activity {
+
+    private class HolderListener implements SurfaceHolder.Callback {
+
+        public void surfaceCreated(SurfaceHolder holder) {
+            try {
+                mCamera.setPreviewDisplay(mView.getHolder());
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            mCamera.stopPreview();
+
+            mCamera.startPreview();
+        }
+
+        public void surfaceDestroyed(SurfaceHolder holder) {
+        }
+    }
+
+    private Camera mCamera;
+    private SurfaceView mView;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -17,6 +44,22 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mView = (SurfaceView)findViewById(R.id.preview);
+        SurfaceHolder holder = mView.getHolder();
+        holder.addCallback(new HolderListener());
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        mCamera = Camera.open();
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mCamera.stopPreview();
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        mCamera.release();
     }
 }
 
