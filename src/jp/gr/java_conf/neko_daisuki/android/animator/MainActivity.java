ed9da2313f96e4f092b01905464c395255ebe43c
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2013-06-25 14:07:17 +0900

    Enhanced to show the camera preview.

--- src/jp/gr/java_conf/neko_daisuki/android/animator/MainActivity.java
+++ src/jp/gr/java_conf/neko_daisuki/android/animator/MainActivity.java
@@ -6,14 +6,18 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Rect;
+import android.hardware.Camera.Parameters;
 import android.hardware.Camera.PictureCallback;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.SurfaceHolder;
@@ -82,7 +86,7 @@ public class MainActivity extends Activity {
         }
     }
 
-    private class HolderListener implements SurfaceHolder.Callback {
+    private class SurfaceListener implements SurfaceHolder.Callback {
 
         public void surfaceCreated(SurfaceHolder holder) {
             try {
@@ -94,14 +98,74 @@ public class MainActivity extends Activity {
         }
 
         public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-            mCamera.stopPreview();
+            String fmt = "the surface was changed: width=%d, height=%d.";
+            Log.d(TAG, String.format(fmt, width, height));
 
+            mCamera.stopPreview();
+            Parameters params = mCamera.getParameters();
+            List<Camera.Size> sizes = params.getSupportedPreviewSizes();
+
+            Camera.Size bestSize = findBestPreviewSize(width, height, sizes);
+            shrinkView(mView, bestSize);
+
+            params.setPreviewSize(bestSize.width, bestSize.height);
+            mCamera.setParameters(params);
             mCamera.startPreview();
         }
 
         public void surfaceDestroyed(SurfaceHolder holder) {
         }
+
+        private void shrinkView(View view, Camera.Size size) {
+            ViewGroup.LayoutParams params = view.getLayoutParams();
+            params.width = size.width;
+            params.height = size.height;
+            view.setLayoutParams(params);
+        }
+
+        private Camera.Size findBestPreviewSize(int width, int height, List<Camera.Size> sizes) {
+            List<Camera.Size> candidates = dropTooLargePreviewSizes(width, height, sizes);
+            if (candidates.size() == 0) {
+                return sizes.get(0);
+            }
+            Camera.Size largestSize = candidates.get(0);
+            int largestArea = largestSize.width * largestSize.height;
+            int len = candidates.size();
+            for (int i = 1; i < len; i++) {
+                Camera.Size size = candidates.get(i);
+                int area = size.width * size.height;
+                if (area < largestArea) {
+                    continue;
+                }
+                largestSize = size;
+                largestArea = area;
+            }
+            return largestSize;
+        }
+
+        private List<Camera.Size> dropTooLargePreviewSizes(int width, int height, List<Camera.Size> sizes) {
+            String fmt = "preview size of width=%d, heigh=%d is %s.";
+
+            List<Camera.Size> l = new LinkedList<Camera.Size>();
+            for (Camera.Size size: sizes) {
+                if ((width < size.width) || (height < size.height)) {
+                    String msg = String.format(
+                            fmt, size.width, size.height, "too large");
+                    Log.d(TAG, msg);
+                    continue;
+                }
+                String msg = String.format(
+                        fmt, size.width, size.height, "enough small");
+                Log.d(TAG, msg);
+
+                l.add(size);
+            }
+
+            return l;
+        }
     }
+
+    private static final String TAG = "animator";
 
     // Document
     private String mProjectDirectory;
@@ -133,7 +197,7 @@ public class MainActivity extends Activity {
 
         mView = (SurfaceView)findViewById(R.id.preview);
         SurfaceHolder holder = mView.getHolder();
-        holder.addCallback(new HolderListener());
+        holder.addCallback(new SurfaceListener());
         holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
         mCamera = Camera.open();
 
