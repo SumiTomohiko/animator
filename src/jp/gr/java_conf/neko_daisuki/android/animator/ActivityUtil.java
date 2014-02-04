ed9f134ea66ab5cd7f3ce201cc3bb6c8c8e4d6d4
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2014-02-04 14:45:53 +0900

    Changed to log exception information.

--- src/jp/gr/java_conf/neko_daisuki/android/animator/ActivityUtil.java
+++ src/jp/gr/java_conf/neko_daisuki/android/animator/ActivityUtil.java
@@ -1,15 +1,21 @@
 package jp.gr.java_conf.neko_daisuki.android.animator;
 
 import android.app.Activity;
+import android.util.Log;
 import android.widget.Toast;
 
 public class ActivityUtil {
+
+    private static final String TAG = "ActivityUtil";
 
     public static void showException(Activity activity,
                                      String msg,
                                      Throwable e) {
         e.printStackTrace();
-        showToast(activity, String.format("%s: %s", msg, e.getMessage()));
+
+        String s = String.format("%s: %s", msg, e.getMessage());
+        showToast(activity, s);
+        Log.e(TAG, s);
     }
 
     public static void showToast(Activity activity, String msg) {
